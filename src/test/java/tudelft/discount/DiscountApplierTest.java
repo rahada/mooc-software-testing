bcbdacb86fcc1aada7a2129f6fc15fb327c64243
rahada <rahadahassan@gmail.com>
2021-07-01 02:58:02 +0200

    addTwoNumbers & DiscountApplier Tests

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,33 @@
 package tudelft.discount;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DiscountApplierTest {
+
+    @Test
+    public void DiscApply(){
+        Product product1 = new Product("name", 25.5, "BUSINESS");
+        Product product2 = new Product("name", 25.5, "HOME");
+        ProductDao dao = new ProductDao() {
+            @Override
+            public List<Product> all() {
+                List<Product> products = new ArrayList<Product>();
+                products.add(product1);
+                products.add(product2);
+
+                return products;
+            }
+        };
+        DiscountApplier discountApplier = new DiscountApplier(dao);
+        discountApplier.setNewPrices();
+
+        Assertions.assertEquals(product1.getPrice(), 22.95);
+        Assertions.assertEquals(product2.getPrice(), 28.05);
+    }
 
 }
--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -3,14 +3,22 @@ package tudelft.sum;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TwoNumbersSumTest {
 
     @Test
     public void TowNumberSum(){
      TwoNumbersSum numbers  = new TwoNumbersSum();
-     int result = numbers.ArrayList();
-        Assertions.assertEquals("1,2",result);
+        ArrayList<Integer> firstArr = new ArrayList<Integer>();
+        firstArr.add(1); firstArr.add(2); firstArr.add(3);
 
+        ArrayList<Integer> secondArr = new ArrayList<Integer>();
+        secondArr.add(4); secondArr.add(5); secondArr.add(6);
+
+        ArrayList<Integer> result = numbers.addTwoNumbers(firstArr, secondArr);
+     Assertions.assertEquals(new ArrayList<Integer>(Arrays.asList(5,7,9)),result);
     }
 
 }
